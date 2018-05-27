01d7cca5ec3488f13d8ad303500f9c8e0e1d7915
Nitai <benshaul.n@gmail.com>
2018-05-27 15:07:17 +0300

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
+import { HomeComponent } from './shared/home/home.component';
+import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    MatIconModule,
+    BrowserModule,
+    RouterModule.forRoot([
+    { path: '', component: HomeComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
